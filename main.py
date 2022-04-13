0f8bd4f0ecb495b69f6c3235d16ecbd84b8ddca8
derkanw <anyaderkachenko@gmail.com>
2022-04-13 15:43:20 +0300

    Add more dependences

--- main.py
+++ main.py
@@ -14,15 +14,15 @@ def f(x: np.ndarray) -> np.ndarray:
     return np.sinc(x)
 
 
-def get_training_data(size: int, left: float, right: float) -> [np.ndarray, np.ndarray]:
+def get_training_data(size: int, left: float, right: float, train_size) -> [np.ndarray, np.ndarray]:
     data = np.linspace(left, right, size)
-    X, _ = train_test_split(data, test_size=0.9)
+    X, _ = train_test_split(data, train_size=train_size)
     return np.atleast_2d(X).T, np.atleast_2d(data).T
 
 
-def evaluate(kernel: Any, m: int, X: np.ndarray, x: np.ndarray, noise: np.ndarray = None) ->\
+def evaluate(kernel: Any, m: int, l: float, X: np.ndarray, x: np.ndarray, noise: np.ndarray = None) ->\
         [Any, np.ndarray, np.ndarray, float, float]:
-    c, l = 1e-5, 0.999
+    c = 1e-5
     mode = "B2P"
 
     krlst = pyKRLST.KRLST(kernel=RBF(kernel), l=l, c=c, M=m, forgetmode=mode)
@@ -34,27 +34,27 @@ def evaluate(kernel: Any, m: int, X: np.ndarray, x: np.ndarray, noise: np.ndarra
     for t, a, b in zip(np.arange(10), X, y):
         krlst.observe(a, b, t)
 
-    y_pred, y_std = krlst.predict(x)
+    y_pred, _ = krlst.predict(x)
 
     mae = MAE(y_pred, f(x))
     rmse = math.sqrt(MSE(f(x), y_pred))
-    return krlst, y_pred, y_std, rmse, mae
+    return krlst, y_pred, y, rmse, mae
 
 
-def evaluate_vector(X: np.ndarray, x: np.ndarray, kernel: Any, isNoisy: bool = False) ->\
+def evaluate_vector(X: np.ndarray, x: np.ndarray, kernel: Any, l: float, isNoisy: bool = False) ->\
         [Any, np.ndarray, np.ndarray, float, float, float]:
-    max_vector_count, index, min_rmse = 50, 0, 100
-    models, preds, stds, rmses, maes = [], [], [], [], []
+    max_vector_count, index, min_rmse = 15, 0, 100
+    models, preds, ys, rmses, maes = [], [], [], [], []
     noise = None
 
     if isNoisy:
-        noise = norm.rvs(scale=0.1, size=len(X))
+        noise = norm.rvs(scale=0.5, size=len(X))
 
     for m in range(max_vector_count):
-        results = evaluate(kernel, m, X, x, noise)
+        results = evaluate(kernel, m, l, X, x, noise)
         models.append(results[0])
         preds.append(results[1])
-        stds.append(results[2])
+        ys.append(results[2])
         rmses.append(results[3])
         maes.append(results[4])
 
@@ -62,37 +62,52 @@ def evaluate_vector(X: np.ndarray, x: np.ndarray, kernel: Any, isNoisy: bool = F
             min_rmse = rmses[m]
             index = m
 
-    return models[index], preds[index], stds[index], rmses[index], maes[index], index
+    return models[index], preds[index], ys[index], rmses[index], maes[index], index
 
 
-def evaluate_kernel(X: np.ndarray, x: np.ndarray) -> [Any, np.ndarray, np.ndarray, float, float, float, float]:
-    models, preds, stds, rmses, maes, ms = [], [], [], [], [], []
-    index, min_rmse, opt_kernel = 0, 100, 0
-    kernel_list = np.linspace(0.001, 1.1, 10)
+def evaluate_kernel(X: np.ndarray, x: np.ndarray, isNoisy: bool = False) ->\
+        [Any, np.ndarray, np.ndarray, float, float, float, float]:
+    models, preds, ys, rmses, maes, ms = [], [], [], [], [], []
+    index, min_rmse, opt_kernel, opt_l = 0, 100, 0, 0
+    kernel_list = np.linspace(0.01, 1.1, 15)
+    l_list = np.linspace(0.1, 1, 15)
 
     for kernel in kernel_list:
-        results = evaluate_vector(X, x, kernel)
-        models.append(results[0])
-        preds.append(results[1])
-        stds.append(results[2])
-        rmses.append(results[3])
-        maes.append(results[4])
-        ms.append(results[5])
+        for l in l_list:
+            results = evaluate_vector(X, x, kernel, l, isNoisy)
+            models.append(results[0])
+            preds.append(results[1])
+            ys.append(results[2])
+            rmses.append(results[3])
+            maes.append(results[4])
+            ms.append(results[5])
 
-        if min_rmse > rmses[len(rmses) - 1]:
-            index = len(rmses) - 1
-            min_rmse = rmses[index]
-            opt_kernel = kernel
+            if min_rmse > rmses[len(rmses) - 1]:
+                index = len(rmses) - 1
+                min_rmse = rmses[index]
+                opt_kernel = kernel
+                opt_l = l
 
-    draw_graph(rmses, "Dependence between kernel parameters and RMSE", "Kernel params", "RMSE", kernel_list)
-    return models[index], preds[index], stds[index], rmses[index], maes[index], ms[index], opt_kernel
+    if not isNoisy:
+        kernel_rmses, l_rmses = [], []
+        for kernel in kernel_list:
+            results = evaluate_vector(X, x, kernel, opt_l)
+            kernel_rmses.append(results[4])
+        draw_graph(kernel_rmses, "Dependence between kernel parameters and RMSE", "Kernel parameters", "RMSE", kernel_list)
+
+        for l in l_list:
+            results = evaluate_vector(X, x, opt_kernel, l)
+            l_rmses.append(results[4])
+        draw_graph(l_rmses, "Dependence between sparsity parameters and RMSE", "Sparsity parameters", "RMSE", l_list)
+
+    return models[index], preds[index], ys[index], rmses[index], maes[index], ms[index], opt_kernel, opt_l
 
 
-def vector_dependence(X: np.ndarray, x: np.ndarray, kernel: Any) -> None:
+def vector_dependence(X: np.ndarray, x: np.ndarray, kernel: Any, l: float) -> None:
     rmses = []
-    max_vector_count = 50
+    max_vector_count = 15
     for m in range(max_vector_count):
-        results = evaluate(kernel, m, X, x)
+        results = evaluate(kernel, m, l, X, x)
         rmses.append(results[3])
     draw_graph(rmses, "Dependence between support vector count and RMSE", "Support vectors", "RMSE")
 
@@ -108,14 +123,12 @@ def draw_graph(values: list, title: str, x_label: str, y_label: str, data: np.nd
     plt.show()
 
 
-def draw_krlst(X: np.ndarray, x: np.ndarray, y_pred: np.ndarray, y_std: np.ndarray, krlst: Any, title: str) -> None:
+def draw_krlst(X: np.ndarray, x: np.ndarray, y_pred: np.ndarray, y: np.ndarray, krlst: Any, title: str) -> None:
     plt.figure(figsize=(10, 5))
     plt.plot(x, f(x), 'r:', label=r'$f(x) = sinc(x)$')
     plt.plot(krlst.Xb, krlst.mu, 'k*', markersize=20, label="Dictionary Elements")
-    plt.plot(X, f(X), 'r.', markersize=15, label='Observations')
+    plt.plot(X, y, 'm.', markersize=10, label='Observations')
     plt.plot(x, y_pred, 'b-', label='Prediction')
-    plt.fill(np.concatenate([x, x[::-1]]), np.concatenate([y_pred - 1.9600 * y_std, (y_pred + 1.9600 * y_std)[::-1]]),
-             alpha=.25, fc='b', ec='None', label='95% confidence interval')
     plt.xlabel('$x$')
     plt.ylabel('$f(x)$')
     plt.ylim(-2, 2)
@@ -125,40 +138,46 @@ def draw_krlst(X: np.ndarray, x: np.ndarray, y_pred: np.ndarray, y_std: np.ndarr
 
 
 def main() -> None:
-    epoch_count, size = 50, 200
-    X, x = get_training_data(200, -2, 2)
+    epoch_count, size, left, right, percent = 15, 200, -2, 2, 0.5
+    rmses = []
+    X, x = get_training_data(size, left, right, percent)
 
     # KRLS-regression without noise
-    model, y_pred, y_std, rmse, mae, m, kernel = evaluate_kernel(X, x)
+    model, y_pred, y, rmse, mae, m, kernel, l = evaluate_kernel(X, x)
+    rmses.append(rmse)
     print("KRLS-regression without noise")
-    print(f"Kernel param = {kernel}")
+    print(f"Kernel parameter = {kernel}")
     print(f"Support vectors = {m}")
+    print(f"Sparsity parameter = {l}")
     print(f"MAE = {mae}, RMSE = {rmse}")
-    draw_krlst(X, x, y_pred, y_std, model, "KRLS-regression without noise")
+    draw_krlst(X, x, y_pred, y, model, "KRLS-regression without noise")
 
-    vector_dependence(X, x, kernel)
+    vector_dependence(X, x, kernel, l)
 
     # KRLS-regression with noise
-    models, preds, stds, rmses, maes, ms = [], [], [], [], [], []
+    models, preds, ys, rmses, maes, ms, kernels, ls = [], [], [], [], [], [], [], []
 
     for epoch in range(epoch_count):
-        results = evaluate_vector(X, x, kernel, True)
+        results = evaluate_kernel(X, x, True)
         models.append(results[0])
         preds.append(results[1])
-        stds.append(results[2])
+        ys.append(results[2])
         rmses.append(results[3])
         maes.append(results[4])
         ms.append(results[5])
+        kernels.append(results[6])
+        ls.append(results[7])
 
     draw_graph(rmses, "RMSE values with using a noise", "Iteration", "RMSE")
 
     index = np.array(rmses).argmin()
     print("\nKRLS-regression with noise and minimum RMSE")
-    print(f"Kernel param = {kernel}")
+    print(f"Kernel parameter = {kernels[index]}")
     print(f"Support vectors = {ms[index]}")
+    print(f"Sparsity parameter = {ls[index]}")
     print(f"MAE = {maes[index]}, RMSE = {rmses[index]}")
     print(f"\nMean RMSE = {np.mean(rmses)}")
-    draw_krlst(X, x, preds[index], stds[index], models[index], "KRLS-regression with noise and minimum RMSE")
+    draw_krlst(X, x, preds[index], ys[index], models[index], "KRLS-regression with noise and minimum RMSE")
 
 
 main()
